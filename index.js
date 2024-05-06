5555d3ee828aa10561b51d4898876b0634dab978
Home <homepc@homes-mbp.myfiosgateway.com>
2024-05-06 16:57:50 -0400

    first commit of nodejs with express

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const port = 5000;
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root:__dirname});
+});
+
+app.listen(port, () => {
+    console.log('now listening to port');
+});
