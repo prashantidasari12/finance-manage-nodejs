d559ec3ee7653da133b1bc76fd6f63aa335b3db4
Home <homepc@homes-mbp.myfiosgateway.com>
2024-05-08 15:20:29 -0400

    added mongoose connection

--- index.js
+++ index.js
@@ -1,13 +1,66 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
-const app = express();
 
-const port = 5000;
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html', {root:__dirname});
-});
+const dbURI= 'mongodb+srv://prashantidasari:YGXk9mSmxJdl1T9g@cluster0.mongodb.net/';
 
-app.listen(port, () => {
-    console.log('now listening to port');
-});
+mongoose.connect(
+   dbURI,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+     
+    const contactSchema = {
+        email: String,
+        query: String,
+    };
+     
+    const Contact =
+        mongoose.model("Contact", contactSchema);
+     
+     
+    app.set("view engine", "ejs");
+     
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+     
+    app.use(express.static(__dirname + '/public'));
+     
+    // app.get("/contact",
+    //     function (req, res) {
+    //         res.render("contact");
+    //     });
+     
+    // app.post("/contact",
+    //     function (req, res) {
+    //         console.log(req.body.email);
+    //         const contact = new Contact({
+    //             email: req.body.email,
+    //             query: req.body.query,
+    //         });
+    //         contact.save(function (err) {
+    //             if (err) {
+    //                 throw err;
+    //             } else {
+    //                 res.render("contact");
+    //             }
+    //         });
+    //     });
+     
+
+    const app = express();
+
+
+    app.get('/', (req, res) => {
+        res.sendFile('index.html', {root:__dirname});
+    });
+
+
+    app.listen(5000,
+        function () {
+            console.log("App is running on Port 3000");
+        });
+
